69bfaae631b224a5adce7b86df2e975452f31aab
Chris Erdos <chris.erdos@gmail.com>
2025-02-13 11:25:51 -0600

    update variables in rectangle tunnel project to be slightly tighter, random rect location to be more offcenter more often

--- projects/complete/rectangle_tunnel.py
+++ projects/complete/rectangle_tunnel.py
@@ -10,13 +10,15 @@ def create_tunnel(plotter: NextDraw):
     starting_width = 0.25
     print(f"Creating tunnel with height: {starting_height}, width: {starting_width}")
 
-    iterations = 35
+    iterations = 40
     print(f"Iterations: {iterations}")
 
-    center_rect_min_x = effective_x_start() + (effective_width() * 0.2)
+    center_rect_min_x = effective_x_start() + (effective_width() * 0.7)
+    center_rect_max_x = effective_x_start() + (effective_width() * 0.9)
+
     center_rect_min_y = effective_y_start() + (effective_height() * 0.6)
-    center_rect_max_x = effective_x_start() + (effective_width() * 0.8)
     center_rect_max_y = effective_y_start() + (effective_height() * 0.8)
+
     center_rect_x = random.uniform(center_rect_min_x, center_rect_max_x)
     center_rect_y = random.uniform(center_rect_min_y, center_rect_max_y)
 
@@ -28,12 +30,12 @@ def create_tunnel(plotter: NextDraw):
     y_max = effective_y_end()
 
     for i in range(iterations):
-        height = starting_height + (i * i * 0.01)
-        width = starting_width + (i * i * 0.01)
+        height = starting_height + (i * i * 0.008)
+        width = starting_width + (i * i * 0.008)
 
         print(f"Iteration: {i + 1} - Rectangle h: {height}, w: {width}")
 
-        if height - starting_height < 0.04 or width - starting_width < 0.04:
+        if height - starting_height < 0.06 or width - starting_width < 0.06:
             print(f"Iteration {i + 1} skip")
             continue
 
