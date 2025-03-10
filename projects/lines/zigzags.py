cf5b9285c3df3a2b587b7d3e49c1ce4905d752ac
Chris Erdos <chris.erdos@gmail.com>
2025-03-10 14:06:09 -0500

    A bit of refactoring

--- projects/lines/zigzags.py
+++ projects/lines/zigzags.py
@@ -18,19 +18,16 @@ def run(plotter: NextDraw, zigs: int):
     zig_width = effective_width() / zigs
     zag_height = effective_height() / 5
     line_distance = zag_height / 5
-    number_of_lines = int(effective_height() / line_distance) * 2  # density
-
     print(
         f"""
     Zig Zags:
     zig width: {zig_width}
     zig zag height: {zag_height}
-    number of vertical lines: {number_of_lines}
     """
     )
 
     end = False
-    for v in range(number_of_lines):
+    while plotter.current_pos()[1] < effective_y_end():
         if end:
             break
 
@@ -48,10 +45,7 @@ def run(plotter: NextDraw, zigs: int):
                     break
                 else:
                     if current_x + zig_width >= effective_x_end() - 0.1:
-                        plotter.penup()
-
                         # reset to starting x
-                        print("Moving to starting X, moving down one line")
                         plotter.moveto(
                             effective_x_start(),
                             plotter.current_pos()[1] + line_distance,
@@ -59,30 +53,21 @@ def run(plotter: NextDraw, zigs: int):
                         break
 
                     # zig down
-                    last_adjusted_y_delta = effective_y_end() - current_y
+                    adjusted_y_delta = effective_y_end() - current_y
                     m = zig_width / zag_height
-                    last_adjusted_x_delta = m * last_adjusted_y_delta
-                    print(
-                        f"zag down: {current_x + last_adjusted_x_delta, effective_y_end()}"
-                    )
-                    plotter.lineto(
-                        current_x + last_adjusted_x_delta, effective_y_end()
-                    )
+                    adjusted_x_delta = m * adjusted_y_delta
+                    plotter.lineto(current_x + adjusted_x_delta, effective_y_end())
 
                     # move right
-                    x_move = (zig_width - last_adjusted_x_delta) * 2
-                    print(f"move right: {x_move, 0}")
+                    x_move = (zig_width - adjusted_x_delta) * 2
                     plotter.move(x_move, 0)
 
-                    print(
-                        f"zag up: {last_adjusted_x_delta, last_adjusted_y_delta * -1}"
-                    )
-                    plotter.line(last_adjusted_x_delta, last_adjusted_y_delta * -1)
+                    # zig up
+                    plotter.line(adjusted_x_delta, adjusted_y_delta * -1)
                     break
             else:
                 if current_x + zig_width >= effective_x_end() - 0.2:
                     plotter.lineto(effective_x_end(), current_y + y_delta)
-                    plotter.penup()
 
                     # reset to starting x
                     print("Moving to starting X, moving down one line")
