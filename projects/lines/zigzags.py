0f8424d649e42abcc6dff523fb705b9bd5ed8c31
Chris Erdos <chris.erdos@gmail.com>
2025-04-30 22:05:53 -0500

    finish lines project

--- projects/lines/zigzags.py
+++ projects/lines/zigzags.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 from nextdraw import NextDraw
 from utils.utils import (
@@ -12,12 +13,14 @@ from utils.utils import (
 
 
 def run(plotter: NextDraw, zigs: int):
+    density_constant = 5
+    zig_width = effective_width() / zigs
+    zag_height = effective_height() / density_constant
+    line_distance = zag_height / density_constant
     start_x = effective_x_start()
     start_y = effective_y_start()
-    plotter.moveto(start_x, start_y)
-    zig_width = effective_width() / zigs
-    zag_height = effective_height() / 5
-    line_distance = zag_height / 5
+    m = zig_width / zag_height
+
     print(
         f"""
     Zig Zags:
@@ -26,16 +29,15 @@ def run(plotter: NextDraw, zigs: int):
     """
     )
 
+    plotter.moveto(start_x, start_y)
     end = False
-    while plotter.current_pos()[1] < effective_y_end():
-        if end:
-            break
-
+    while plotter.current_pos()[1] < effective_y_end() and not end:
         for i in range(zigs):
             current_x = plotter.current_pos()[0]
             current_y = plotter.current_pos()[1]
             y_delta = zag_height if i % 2 == 0 or i == 0 else zag_height * -1
 
+            # Past the bottom
             if current_y + y_delta >= effective_y_end():
                 if current_y - effective_y_end() > 0.1:
                     # end
@@ -52,19 +54,16 @@ def run(plotter: NextDraw, zigs: int):
                         )
                         break
 
-                    # zig down
                     adjusted_y_delta = effective_y_end() - current_y
-                    m = zig_width / zag_height
                     adjusted_x_delta = m * adjusted_y_delta
                     plotter.lineto(current_x + adjusted_x_delta, effective_y_end())
 
-                    # move right
                     x_move = (zig_width - adjusted_x_delta) * 2
                     plotter.move(x_move, 0)
 
-                    # zig up
                     plotter.line(adjusted_x_delta, adjusted_y_delta * -1)
                     break
+            # The middle
             else:
                 if current_x + zig_width >= effective_x_end() - 0.2:
                     plotter.lineto(effective_x_end(), current_y + y_delta)
@@ -78,3 +77,27 @@ def run(plotter: NextDraw, zigs: int):
                     break
 
             plotter.line(zig_width, y_delta)
+
+    # top section
+    # Above the top
+    plotter.moveto(effective_x_start(), effective_y_start())
+    for z in range(int(zigs / 2)):
+        x_step_distance = zig_width / density_constant
+
+        for i in range(density_constant):
+            plotter.move(x_step_distance, 0)
+            adjusted_x_delta = zig_width - (x_step_distance * (i + 1))
+
+            adjusted_y_delta = zag_height - ((zag_height / density_constant) * (i + 1))
+
+            # zig down
+            plotter.line(adjusted_x_delta, adjusted_y_delta)
+
+            # zag up
+            plotter.line(adjusted_x_delta, adjusted_y_delta * - 1)
+
+            # reset
+            if i + 1 != density_constant:
+                plotter.move((adjusted_x_delta * - 2), 0)
+
+        plotter.move(zig_width, 0)
